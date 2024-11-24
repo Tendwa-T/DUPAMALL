d25a16c273fad7dd64f98b454efebbc48fe2090e
Michemor <yatoijema@gmail.com>
2024-11-24 15:14:41 +0300

    API for login

--- login.php
+++ login.php
@@ -8,31 +8,45 @@ require_once 'connect_db.php';
 
 $contact = $_GET['contact'];
 $password = $_GET['password'];
+
+if(!$contact || !$password){
+    echo json_encode(['code' => 0, 'message' => 'Contact and password required']);
+    exit;
+}
 // Query to fetch user records based on phone and password
-$query = $conn->prepare("SELECT seller_contact, seller_password FROM seller WHERE seller_contact = ? AND seller_password = ?");
-$query->bind_param("ss", $contact, $password);
+$query = $conn->prepare("SELECT * FROM seller WHERE seller_contact = ?");
+$query->bind_param("s", $contact, );
 $query->execute();
 $result = $query->get_result();
 
 // if the number of rows is greater than 1, then we do have a user present
 if ($result->num_rows > 0) {
  // Fetch user details
- $rows = [];
- while ($row = $result->fetch_assoc()) {
- $rows[] = $row;
+ $user = $result->fetch_assoc();
+ 
+
+
+ // check whether the user has the same password as the one
+ if ($password === $user['seller_password']) {
+    echo json_encode([
+        'code' => 1,
+        'message' => 'Login successful.',
+        'userdetails' => $user
+    ]);
+
+ } else {
+    echo json_encode([
+        'code' => 0,
+        'message' => "Invalid Password"]);
  }
- // Return success response with user details
- echo json_encode([
- 'code' => 1,
- 'userdetails' => $rows
- ]);
-} else {
+ } else {
+    echo json_encode([
+        'code' => 0,
+        'message' => 'User not found',
+    ]);
  // Return error response if credentials are invalid
- echo json_encode([
- 'code' => 0,
- 'message' => 'Invalid contact or password.'
- ]);
-}
+ }
 $query->close();
+
 mysqli_close($conn);
 ?>
