81d82178c9d8ff8b7d854a0b630ac7405408cf89
Michemor <yatoijema@gmail.com>
2024-11-25 18:38:16 +0300

    modified how product details are saved when fetching from url due to trim deprecation error

--- backend/createproduct.php
+++ backend/createproduct.php
@@ -10,12 +10,12 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
 
     $product = json_decode(file_get_contents('php://input'), true);
 
-    $product_name = trim($product['product_name']);
-    $product_price = trim($product['product_price']);
-    $product_description = trim($product['product_description']);
-    $product_type = trim($product['product_type']);
-    //image is stored as a string that is the url of the image
-    $product_image = trim($product['product_image']);
+   $product_name = isset($product['product_name']) ? trim($product['product_name']) : '';
+    $product_price = isset($product['product_price']) ? trim($product['product_price']) : '';
+    $product_description = isset($product['product_description']) ? trim($product['product_description']) : '';
+    $product_type = isset($product['product_type']) ? trim($product['product_type']) : '';
+    $product_image = isset($product['product_image']) ? trim($product['product_image']) : '';
+
 
     if(empty($product_name) || empty($product_price) || empty($product_description) || empty($product_type) || empty($product_image))
     {
@@ -23,8 +23,8 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
     } else{
 
         //inserting data into the database (in robots voice)
-        $stmt = $conn->prepare("INSERT INTO product (product_name, product_price, product_description, product_type, product_image) VALUES (?, ?, ?, ?, ?)");
-        $stmt->bind_param("sssss", $product_name, $product_price, $product_description, $product_type, $product_image);
+        $stmt = $conn->prepare("INSERT INTO product (product_name, product_description, product_type, product_quantity, product_price, product_image) VALUES (?, ?, ?, ?, ?, ?)");
+        $stmt->bind_param("ssssss", $product_name, $product_description, $product_type, $product_quantity, $product_price, $product_image);
 
         if ($stmt->execute()) {
             echo json_encode(['code' => 1, 'message' => 'Product created successfully']);
